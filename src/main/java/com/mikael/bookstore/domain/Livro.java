d7360fd4adcbb8b9a426a4e19ecac0c4f8e0b629
Mikael-GIT <mikael.tavares@unigranrio.br>
2021-04-09 01:38:20 -0300

    Classes de domínios iniciadas

---
+++ src/main/java/com/mikael/bookstore/domain/Livro.java
@@ -0,0 +1,14 @@
+package com.mikael.bookstore.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Livro {
+	private Integer id;
+	private String titulo;
+	private String nomeAutor;
+	private String texto;
+	
+	private Categoria categoria;
+	
+}
